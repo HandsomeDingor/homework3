e0c1543426911ca4310d6c982506cf56451fb93f
HandsomeDingor <jay807541931@gmail.com>
2022-01-04 01:51:21 -0500

    add var and window prompt

--- Develop/script.js
+++ Develop/script.js
@@ -13,5 +13,48 @@ function writePassword() {
 
 }
 
+var upperLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+var lowerLetters = 'abcdefghijklmnopqrstuvwxyz';
+var numbers = '0123456789';
+var symbols = '!"#$%&()*+,-./:;<=>?@[^_`{|}~';
+var isLowercase = true;
+var isUppercase = true;
+var isNumberic = true;
+var isSpecial = true;
+
+function generatePassword () {
+  password_length = prompt("Please enter the length of Password from 8 to 128");
+  if (password_length >=8 && password_length<= 128){
+  }
+  else{
+    alert ("Please enter the length of Password from 8 to 128")
+  }
+
+    isLowercase = confirm("Password includes lowercase");
+		isUppercase = confirm("Password includes uppercase");
+		isNumberic = confirm("Password includes numbers");
+		isSpecial = confirm("Password includes special characters");
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
