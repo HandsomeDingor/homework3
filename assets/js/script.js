acb492df62d227fea2c12968457a57bb57b0a7bf
HandsomeDingor <jay807541931@gmail.com>
2022-01-06 21:30:28 -0500

    fix bug

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,53 +22,58 @@ var isUppercase = true;
 var isNumberic = true;
 var isSpecial = true;
 
-function generatePassword () {
+function generatePassword() {
 
-  do{
+  do {
     var password_length = prompt("Please enter the length of Password");
     //check if hit cancel
-    if(password_length === null){
-      alert ("Please enter number form 8 to 128 or close window");
-    } else if (isNaN(password_length)){ //check string or number
-      alert ("Please enter number only");
-    } else if (password_length < 8 || password_length > 128){ //check number interval 
-      alert ("number from 8 to 128");
-    }
-  }while(!(!(isNaN(password_length)) && password_length >=8 && password_length <= 128))
+    if (password_length === null) {
+      alert("Please enter number form 8 to 128 or close window");
+    } else if (password_length === "") {
+      alert("Please enter number form 8 to 128 or close window");
+    } else if (isNaN(password_length)) { //check string or number
+      alert("Please enter number only");
 
-  do{
+    } else if (password_length < 8 || password_length > 128) { //check number interval 
+      alert("number from 8 to 128");
+    }
+
+    console.log(password_length);
+  } while (!(!(isNaN(password_length)) && password_length >= 8 && password_length <= 128))
+
+  do {
     //check if true or false
     isLowercase = confirm("Password includes lowercase");
     isUppercase = confirm("Password includes uppercase");
     isNumberic = confirm("Password includes numbers");
     isSpecial = confirm("Password includes special characters");
 
-    if(!isLowercase&&!isUppercase&&!isNumberic&&!isSpecial){
-			alert("at least one character type should be selected");
-		}
+    if (!isLowercase && !isUppercase && !isNumberic && !isSpecial) {
+      alert("at least one character type should be selected");
+    }
 
     var chars = "";
     var pw = ""
 
     //check and add up the chars string
-    if (isLowercase){
-      chars = chars + lowerLetters ;
+    if (isLowercase) {
+      chars = chars + lowerLetters;
     }
-    if (isUppercase){
+    if (isUppercase) {
       chars = chars + upperLetters;
     }
-    if (isNumberic){
+    if (isNumberic) {
       chars = chars + numbers;
     }
-    if (isSpecial){
+    if (isSpecial) {
       chars = chars + symbols;
     }
-  }while (!isLowercase && !isUppercase && !isNumberic && !isSpecial)
+  } while (!isLowercase && !isUppercase && !isNumberic && !isSpecial)
 
   // using for loop to random select letter for "chars string"
-  for (var i=0; i< password_length; i++){
+  for (var i = 0; i < password_length; i++) {
     pw += chars.charAt(Math.floor(Math.random() * chars.length));
-  } 
+  }
 
   return pw;
 }
